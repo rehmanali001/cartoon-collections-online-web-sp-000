132d25039bcfe67980e386cfde350fa88759c412
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 22:31:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def find_the_cheese(string)
   string.each do |line|
-  cheese_types.include?(line)
+  if cheese_types.include?(line)
     return line
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
