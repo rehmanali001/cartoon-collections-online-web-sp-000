e6bb2e77269e51d3c8764944306105fe9ab14df0
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 21:37:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.map {}
+  veggies.map { |name| name.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
