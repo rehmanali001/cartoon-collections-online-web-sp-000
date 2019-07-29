d38859551e37577ff454933564b73528b4e4e5e1
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 22:08:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ end
 def summon_captain_planet(veggies)
   veggies.map {|name| name.capitalize + '!'}
 end
-def long_planeteer_calls()
-  
+def long_planeteer_calls(array)
+  if array.length > 4
+    true
+  else 
+    false
 end
 
 def find_the_cheese# code an argument here
